ac929235bcba762244c0f8f252626cc3e888fa63
cra2ycoder <49195865+cra2ycoder@users.noreply.github.com>
2021-05-12 21:23:35 +0530

    docsify setup enabled for css tutorial

---
+++ plugin.js
@@ -0,0 +1,11 @@
+const sirvURL = 'https://cra2ycoder.sirv.com/'
+
+const plugins = [
+  function (hook, vm) {
+    hook.ready(function () {
+      new Zooming({
+        // options...
+      }).listen('.img-zoomable')
+    })
+  },
+]
