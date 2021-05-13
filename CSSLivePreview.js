8fe29e13b123135eceab271dc8053df0d3eebddb
cra2ycoder <49195865+cra2ycoder@users.noreply.github.com>
2021-05-13 07:35:45 +0530

    trial codes

--- CSSLivePreview.js
+++ CSSLivePreview.js
@@ -1,5 +1,19 @@
-function CSSLivePreview() {
+function CSSLivePreview(settings) {
   this.parent = null
+  this.type = settings.type || 'radio'
+  this.config = settings.config
+}
+
+CSSLivePreview.prototype.createPanels = () => {
+  this.parent.innerHTML = `
+    <form>
+      <textarea id="htmlString" />
+      <textarea id="cssString" />
+      <div class="options-panel">
+        ${this.config.options.map(x => `<input id="${x.label}" />`)}
+      </div>
+    </form>
+  `
 }
 
 CSSLivePreview.prototype.init = id => {
--- plugin.js
+++ plugin.js
@@ -1,10 +1,17 @@
-const sirvURL = 'https://cra2ycoder.sirv.com/'
-
 const plugins = [
   function (hook, vm) {
     hook.doneEach(function () {
-      if (typeof CSSLivePreview === 'function') {
-        new CSSLivePreview().init('css-attributes')
+      console.log('yessss')
+      try {
+        if (typeof CSSLivePreview === 'function') {
+          new CSSLivePreview({
+            config: {
+              options: ['[attr]', '[attr=val]', '[attr~=val]'],
+            },
+          }).init('css-attributes')
+        }
+      } catch (e) {
+        console.log({ e })
       }
     })
   },
