11246cb1a7fc78f39c148216b1811b216e4a8642
cra2ycoder <49195865+cra2ycoder@users.noreply.github.com>
2021-05-16 22:26:17 +0530

    updates - removed unused files - linkedin link updated

--- plugin.js
+++ plugin.js
@@ -1,18 +1,5 @@
 const plugins = [
   function (hook, vm) {
-    hook.doneEach(function () {
-      console.log('yessss')
-      try {
-        if (typeof CSSLivePreview === 'function') {
-          new CSSLivePreview({
-            config: {
-              options: ['[attr]', '[attr=val]', '[attr~=val]'],
-            },
-          }).init('css-attributes')
-        }
-      } catch (e) {
-        console.log({ e })
-      }
-    })
+    hook.doneEach(function () {})
   },
 ]
